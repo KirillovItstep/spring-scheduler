8c12377ea8e6cac5572bac0c6144e853978e515d
kirillovItStep <kirillov.malp@gmail.com>
2023-07-29 19:26:14 +0300

    Spring-scheduler

---
+++ scheduling/src/main/java/org/itstep/async/config/SpringAsyncConfig.java
@@ -0,0 +1,34 @@
+package org.itstep.async.config;
+
+import java.util.concurrent.Executor;
+import org.itstep.async.CustomAsyncExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync()
+@ComponentScan("org.itstep")
+public class SpringAsyncConfig implements AsyncConfigurer {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new CustomAsyncExceptionHandler();
+    }
+}
